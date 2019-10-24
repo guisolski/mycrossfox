44cb79d7b822ee8564f3e974e7cee0bce6ba20a2
Bruno Thuma <40045005+BrunoThuma@users.noreply.github.com>
2019-10-24 10:59:41 -0300

    Ajustes Front tela Login
    Retirado pasta do Semanthic
    Apaga texto de erro do login ao inserir email e/ou senha validos

--- script/login.js
+++ script/login.js
@@ -4,12 +4,13 @@ $(document).ready(function () {
 
 
     function proximo() {
-        if ($("#input_login").val() != "") {
+        if ($("#input_login").val().indexOf('@') > 0) {
             $("#nome").html('<i class="fa fa-arrow-left"></i> ' + $("#input_login").val());
             $("#login").hide();
             $("#senha").removeClass("invisible");
             $("#input_login").removeClass("error");
             $("#senha").show();
+            $("#titulo_login #msg_erro_login").remove()
         }
         else {
             $("#titulo_login #msg_erro_login").remove()
@@ -33,9 +34,11 @@ $(document).ready(function () {
         }
     });
 
-    $("#entrar").click(function () {
+    function entrarSenha(){
         if ($("#input_senha").val() != "") {
             alert("mandar pro servidor");
+            $("#input_senha").removeClass("error")
+            $("#titulo_senha #msg_erro_senha").remove()
         }
         else {
             $("#titulo_senha #msg_erro_senha").remove()  
@@ -43,17 +46,15 @@ $(document).ready(function () {
             $("#titulo_senha").append("<label id='msg_erro_senha' class='text-danger'>Sua conta ou senha está incorreta.<br>"+
               "Se você não se lembra de sua senha redefina-a em 'Esqueceu a senha?'</label>")
         }
+    }
+
+    $("#entrar").click(function () {
+        entrarSenha();
     });
 
     $("#input_senha").keypress(function (e) {
         if (e.keyCode == 13) {
-            if ($("#input_senha").val() != "") {
-                alert("mandar pro servidor");
-            }
-            else {
-                $("#input_senha").addClass("error");
-                alert("Entre com um valor");
-            }
+            entrarSenha();
         }
     });
 
