d5e8617bed0e82c2f069a22c83e356ecda945882
Guilherme Solski Alves <guigusolski@gmail.com>
2019-11-13 16:15:22 -0300

    arrumando login.php

--- php/login.php
+++ php/login.php
@@ -1,17 +1,29 @@
 <?php
-	
     $usuario = $_POST["usuario"];
     $senha = $_POST["senha"];
-
 	//Inicio Leitura XML
-	$retorno = "";
 
 	$xmlString = file_get_contents("../xml/base_usuarios.xml");
 	$xml = simplexml_load_string($xmlString);
-
+	$valido = false;
+	
 	foreach($xml->usuario as $u){
 		if ($u == $usuario && $senha = $u["senha"]){
-            echo"Valido";
+            $valido = true;
         }
 	}
+	
+	
+	if($valido){
+		$_SESSION['usuario'] = $usuario;
+		$_SESSION['senha'] = $senha;
+		$retorno["link"] =  trim("email.html");
+		echo json_encode($retorno);
+	}
+	else{
+		unset ($_SESSION['usuario']);
+		unset ($_SESSION['senha']);
+		$retorno["link"] =  trim("../index.html");
+		echo json_encode($retorno);
+	}	
 ?>
