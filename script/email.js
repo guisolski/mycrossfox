a0fecd4bfd965d282b57f2bbc49b8e3717599474
Guilherme Solski Alves <guigusolski@gmail.com>
2019-11-14 13:55:21 -0300

    Update email.js

--- script/email.js
+++ script/email.js
@@ -1,10 +1,30 @@
-$(document).ready(function() {
-    $("#pesquisa").focus(function() {
+$(document).ready(function () {
+    var usuario = JSON.parse(sessionStorage.getItem("usuario"));
+    $("#overlay").fadeIn();
+    $("#pesquisa").focus(function () {
         $("#div_pesquisa").removeClass("col-sm-2").addClass("col-sm-5");
         $("#vazio").removeClass("col-sm-5").addClass("col-sm-2");
     });
-    $("#pesquisa").focusout(function() {
+    $("#pesquisa").focusout(function () {
         $("#div_pesquisa").removeClass("col-sm-5").addClass("col-sm-2");
         $("#vazio").removeClass("col-sm-2").addClass("col-sm-5");
+    });
+    $.ajax({
+        type: "GET",
+        url: "../php/email.php",
+        data: {
+            login   : usuario.login,
+            senha   : usuario.senha,
+            action  : "list_emails",
+            type    : "recived"
+        },
+        success: function (data) {
+            var resultado = JSON.parse(data);
+            console.log(resultado);
+            $("#overlay").fadeOut();
+        },
+        error: function () {
+            alert("erro");
+        }
     });
 });
